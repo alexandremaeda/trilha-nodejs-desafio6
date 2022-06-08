455b1a6099fecc3e5aa8cc0f7fa700a19f71a073
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-06-08 20:51:32 -0300

    transfer route

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -1,20 +1,27 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { CreateStatementController } from '../modules/statements/useCases/createStatement/CreateStatementController';
-import { GetBalanceController } from '../modules/statements/useCases/getBalance/GetBalanceController';
-import { GetStatementOperationController } from '../modules/statements/useCases/getStatementOperation/GetStatementOperationController';
-import { ensureAuthenticated } from '../shared/infra/http/middlwares/ensureAuthenticated';
+import { CreateStatementController } from "../modules/statements/useCases/createStatement/CreateStatementController";
+import { CreateStatementTransferController } from "../modules/statements/useCases/createStatementTransfer/CreateStatementTransferController";
+import { GetBalanceController } from "../modules/statements/useCases/getBalance/GetBalanceController";
+import { GetStatementOperationController } from "../modules/statements/useCases/getStatementOperation/GetStatementOperationController";
+import { ensureAuthenticated } from "../shared/infra/http/middlwares/ensureAuthenticated";
 
 const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
+const createStatementTransferController =
+  new CreateStatementTransferController();
 const getStatementOperationController = new GetStatementOperationController();
 
 statementRouter.use(ensureAuthenticated);
 
-statementRouter.get('/balance', getBalanceController.execute);
-statementRouter.post('/deposit', createStatementController.execute);
-statementRouter.post('/withdraw', createStatementController.execute);
-statementRouter.get('/:statement_id', getStatementOperationController.execute);
+statementRouter.get("/balance", getBalanceController.execute);
+statementRouter.post("/deposit", createStatementController.execute);
+statementRouter.post("/withdraw", createStatementController.execute);
+statementRouter.post(
+  "/transfer/:sender_id",
+  createStatementTransferController.execute
+);
+statementRouter.get("/:statement_id", getStatementOperationController.execute);
 
 export { statementRouter };
