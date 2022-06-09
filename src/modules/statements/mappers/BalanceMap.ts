643354896695a8d27ff9f35269cbb201583e518e
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-06-08 21:27:33 -0300

    balancemap sernder_id

--- src/modules/statements/mappers/BalanceMap.ts
+++ src/modules/statements/mappers/BalanceMap.ts
@@ -1,28 +1,36 @@
-import { Statement } from "../entities/Statement";
+import { Statement } from '../entities/Statement';
 
 export class BalanceMap {
-  static toDTO({statement, balance}: { statement: Statement[], balance: number}) {
-    const parsedStatement = statement.map(({
-      id,
-      amount,
-      description,
-      type,
-      created_at,
-      updated_at
-    }) => (
-      {
+  static toDTO({
+    statement,
+    balance,
+  }: {
+    statement: Statement[];
+    balance: number;
+  }) {
+    const parsedStatement = statement.map(
+      ({
         id,
+        sender_id,
+        amount,
+        description,
+        type,
+        created_at,
+        updated_at,
+      }) => ({
+        id,
+        sender_id,
         amount: Number(amount),
         description,
         type,
         created_at,
-        updated_at
-      }
-    ));
+        updated_at,
+      })
+    );
 
     return {
       statement: parsedStatement,
-      balance: Number(balance)
-    }
+      balance: Number(balance),
+    };
   }
 }
